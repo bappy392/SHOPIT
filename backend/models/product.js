17da1cf210a7a8efe6969db8be2a88765ffdb65b
mahmudul <bappy392@gmail.com>
2022-03-30 18:13:59 +0600

    f

--- backend/models/product.js
+++ backend/models/product.js
@@ -9,13 +9,82 @@ const productSchema=new mongoose.Schema({
     price:{
         type:Number,
         required:[true, 'Please enter product price'],
-        maxLenght:[1500,'Product name cannot exceed 5 charecters'],
+        maxLenght:[1500,'Product price cannot exceed 5 charecters'],
         default:0.0
     },    
     description:{
         type:String,
         required:[true, 'Please enter description'],
     },
+    ratings:{
+        type:Number,
+        default:0
+    },
+    images:[{
+        public_id:{
+            type:String,
+            required:true
+        },
+        ulr:{
+            type:String,
+            required:true
+        }
+    }],
+    category:{
+        type:String,
+        required:[true,'Please select category for this product'],
+        enum:{
+            values:[
+                'Electronics',
+                'Cameras',
+                'Laptop',
+                'Accessories',
+                'Headphones',
+                'Food',
+                'Books',
+                'Clothes/Shoes',
+                'Beauty/Health',
+                'Sports',
+                'Outdoor',
+                'Home'
+            ],
+            message:'Please select correct category for product'
+        }
+    },
+    seller:{
+        type:String,
+        required:[true,'Please enter product seller']
+    },
+    stock:{
+        type:Number,
+        required:[true,'Please enter product stock'],
+        maxLenght:[5,'Product stock cannot exceed 5 characters'],
+        default:0
+    },
+    numofReviews:{
+        type:Number,
+        default:0
+    },
+    reviews:[
+        {
+            name:{
+                type:String,
+                required:true
+            },
+            ratting:{
+                type:Number,
+                required:true
+            },
+            comment:{
+                type:String,
+                required:true
+            }
+        }
+    ],
+    createdAt:{
+        type:Date,
+        default:Date.now
+    }
 })
 
 module.exports=mongoose.model('Product',productSchema)
